aeaac7063438256a5adaf6654fefcdbd907d55b5
samandarsharipov2214 <samandarsharipov22@gmail.com>
2026-07-08 19:30:05 +0300

    Add iOS SwiftUI app entrypoint

---
+++ ios/SharipovAI/SharipovAIApp.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+@main
+struct SharipovAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            RootView()
+                .preferredColorScheme(.dark)
+        }
+    }
+}
