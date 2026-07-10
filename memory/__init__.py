3d1b55d5852f73f63e86e0ff7217604c2d427307
samandarsharipov2214 <samandarsharipov22@gmail.com>
2026-07-10 20:13:20 +0300

    feat(memory): expose unified and market impact memory

--- memory/__init__.py
+++ memory/__init__.py
@@ -1,6 +1,15 @@
 """Persistent memory utilities for SharipovAI OS."""
 
+from .market_impact_memory import MarketImpact, MarketImpactMemory
 from .memory_engine import MemoryEngine
 from .models import DecisionRecord
+from .unified_memory import MemoryItem, UnifiedMemory
 
-__all__: tuple[str, ...] = ("DecisionRecord", "MemoryEngine")
+__all__: tuple[str, ...] = (
+    "DecisionRecord",
+    "MarketImpact",
+    "MarketImpactMemory",
+    "MemoryEngine",
+    "MemoryItem",
+    "UnifiedMemory",
+)
