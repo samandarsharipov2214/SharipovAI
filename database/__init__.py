f7b55c2278503b0cc945fc5bce9f7f79506359bd
samandarsharipov2214 <samandarsharipov22@gmail.com>
2026-07-12 02:59:04 +0300

    chore(database): remove duplicate package entrypoint

--- database/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Unified PostgreSQL data layer for SharipovAI."""
-
-from .unified_store import DatabaseConfigurationError, UnifiedStore, validate_database_url
-
-__all__ = ("DatabaseConfigurationError", "UnifiedStore", "validate_database_url")
