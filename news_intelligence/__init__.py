687886e5053e0d90dcda9a2743fd5371d773dec8
samandarsharipov2214 <samandarsharipov22@gmail.com>
2026-07-13 14:42:20 +0300

    fix(news): restore News Intelligence package exports

---
+++ news_intelligence/__init__.py
@@ -0,0 +1,18 @@
+"""Canonical, database-backed News Intelligence package."""
+from .agents import SourceAgent
+from .hub import NewsHub
+from .models import NewsArticle, NewsEnvelope, SourceFetch
+from .network import NewsAgentNetwork
+from .sources import SourceCollector, SourceDefinition, source_definitions
+
+__all__ = [
+    "NewsAgentNetwork",
+    "NewsArticle",
+    "NewsEnvelope",
+    "NewsHub",
+    "SourceAgent",
+    "SourceCollector",
+    "SourceDefinition",
+    "SourceFetch",
+    "source_definitions",
+]
