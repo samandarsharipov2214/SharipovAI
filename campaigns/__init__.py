9afa35445b13fbb0c9d588095231507490265a15
samandarsharipov2214 <samandarsharipov22@gmail.com>
2026-07-14 18:52:49 +0300

    feat: export scheduled campaign services

---
+++ campaigns/__init__.py
@@ -0,0 +1,14 @@
+"""Scheduled Testnet campaign orchestration and final promotion evidence."""
+from .core import (
+    FinalPromotionReportEngine,
+    ScheduledCampaignOrchestrator,
+    ShadowCampaignPolicy,
+    TestnetShadowCampaign,
+)
+
+__all__ = [
+    "FinalPromotionReportEngine",
+    "ScheduledCampaignOrchestrator",
+    "ShadowCampaignPolicy",
+    "TestnetShadowCampaign",
+]
