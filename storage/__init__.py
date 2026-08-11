f4430191b637b8f673133d4cb2d4d679f2ba160a
samandarsharipov2214 <samandarsharipov22@gmail.com>
2026-08-11 19:45:23 +0300

    perf: export canonical record counter

--- storage/__init__.py
+++ storage/__init__.py
@@ -1,6 +1,6 @@
 """Canonical persistence layer for SharipovAI."""
 from .change_ledger import ProjectChangeLedger
-from .collections import list_json_items
+from .collections import count_json_items, list_json_items
 from .domain_store import ProjectDomainStore, StoredRecord
 from .project_database import DatabaseUnavailable, ProjectDatabase, VersionConflict
 
@@ -11,5 +11,6 @@ __all__ = [
     "ProjectDomainStore",
     "StoredRecord",
     "VersionConflict",
+    "count_json_items",
     "list_json_items",
 ]
